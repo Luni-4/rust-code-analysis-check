eb11630654684a6c3e0992ad9a211bcd0fc198b2
Luni-4 <luni-4@hotmail.it>
2020-11-03 19:27:13 +0100

    Add code to parse subspaces

--- src/check.ts
+++ src/check.ts
@@ -7,14 +7,17 @@ const pkg = require('../package.json');
 // Get user agent from package data
 const USER_AGENT = `${pkg.name}/${pkg.version} (${pkg.bugs.url})`;
 
+// Define a new type for the annotations
+type ChecksCreateParamsOutputAnnotations = any;
+
 // Json structure of the files returned by rust-code-analysis-cli
 interface RcaFile {
-  name: string,
-  start_line: number,
-  end_line: number,
+  filepath: string,
+  line_start: number,
+  line_end: number,
   kind: string,
   metrics: Metrics,
-  spaces: any, // FIXME: To be more granular, we need to define this
+  spaces: any, // This field is a JSON array, containing other JSON arrays
 }
 
 // Metrics structure
@@ -73,13 +76,16 @@ interface CheckOptions {
 }
 
 export class CheckRunner {
+    // Array containing spaces annotations
+    private annotations: Array<ChecksCreateParamsOutputAnnotations>;
     // Array containing the metrics for each file
     private file_metrics: Array<string>;
-    // Array containing deserialized Json files
+    // Array containing deserialized Json files for pull requests
     private file_jsons: Array<RcaFile>;
 
     // Create a new CheckRunner
     constructor() {
+        this.annotations = [];
         this.file_metrics = [];
         this.file_jsons = [];
     }
@@ -95,11 +101,16 @@ export class CheckRunner {
             return;
         }
 
-        // Save json files
-        this.file_jsons.push(contents);
+        // Save json files for pull requests
+        this.file_jsons.push(Object.assign({}, contents));
 
-        // Save metrics extracted from the json in an array
+        // Save metrics extracted from the json object in an array
         this.file_metrics.push(this.getFileMetrics(contents));
+
+        // Create space annotations (if there is at least one space)
+        if (contents.spaces.length > 0) {
+            this.makeAnnotations(contents);
+        }
     }
 
     // Execute the check and display the results produced by rust-code-analysis
@@ -136,8 +147,14 @@ See https://github.com/Luni-4/rust-code-analysis-check#limitations for details.`
         }
 
         try {
-            // Successful check, so display metrics
-            await this.successCheck(client, checkRunId, options);
+            // Check if there are annotations
+            if (this.annotations.length == 0) {
+                // Display only global metrics
+                await this.globalMetricsCheck(client, checkRunId, options);
+            } else {
+                // Display global and space metrics
+                await this.allMetricsCheck(client, checkRunId, options);
+            }
         } catch (error) {
             // Cancel check
             await this.cancelCheck(client, checkRunId, options);
@@ -159,8 +176,8 @@ See https://github.com/Luni-4/rust-code-analysis-check#limitations for details.`
         return response.data.id;
     }
 
-    // Conclude the whole check when the outcome is successful
-    private async successCheck(client: any, checkRunId: number, options: CheckOptions): Promise<void> {
+    // Check to display only global metrics
+    private async globalMetricsCheck(client: any, checkRunId: number, options: CheckOptions): Promise<void> {
         let req: any = {
             owner: options.owner,
             repo: options.repo,
@@ -178,6 +195,48 @@ See https://github.com/Luni-4/rust-code-analysis-check#limitations for details.`
 
         // TODO: Check for errors
         await client.checks.update(req);
+
+        return;
+    }
+
+    // Check to display both global and space metrics
+    private async allMetricsCheck(client: any, checkRunId: number, options: CheckOptions): Promise<void> {
+
+        // Checks API allows only up to 50 annotations per request,
+        // should group them into buckets
+        let annotations = this.getBucket();
+        while (annotations.length > 0) {
+            // Request data is mostly the same for create/update calls
+            let req: any = {
+                owner: options.owner,
+                repo: options.repo,
+                name: options.name,
+                check_run_id: checkRunId,
+                output: {
+                    title: options.name,
+                    summary: '',
+                    text: this.getText(options.rca_version),
+                    annotations: annotations,
+                }
+            };
+
+            if (this.annotations.length > 0) {
+                // There will be more annotations later
+                core.debug('This is not the last iteration, marking check as "in_progress"');
+                req.status = 'in_progress';
+            } else {
+                // Okay, that was a last one bucket
+                req.status = 'completed';
+                req.conclusion = 'success';
+                req.completed_at = new Date().toISOString();
+            }
+
+            // TODO: Check for errors
+            await client.checks.update(req);
+
+            // Get next annotation bucket
+            annotations = this.getBucket();
+        }
 
         return;
     }
@@ -205,6 +264,23 @@ See https://github.com/Luni-4/rust-code-analysis-check#limitations for details.`
         return;
     }
 
+    // Create a bucket of 50 annotations
+    private getBucket(): Array<ChecksCreateParamsOutputAnnotations> {
+        let annotations: Array<ChecksCreateParamsOutputAnnotations> = [];
+        while (annotations.length < 50) {
+            const annotation = this.annotations.pop();
+            if (annotation) {
+                annotations.push(annotation);
+            } else {
+                break;
+            }
+        }
+
+        core.debug(`Prepared next annotations bucket, ${annotations.length} size`);
+
+        return annotations;
+    }
+
     // Print a json file to stdout
     private dumpToStdout() {
         for (const json_metric of this.file_jsons) {
@@ -212,6 +288,49 @@ See https://github.com/Luni-4/rust-code-analysis-check#limitations for details.`
             core.info(JSON.stringify(json_metric, null, 2));
         }
     }
+
+    // Convert spaces information into GitHub annotation objects
+    //
+    // https://developer.github.com/v3/checks/runs/#annotations-object
+    private makeAnnotations(contents: RcaFile) {
+        // NOTE: We can use recursion because we know beforehand that
+        // ends and there are not so many levels.
+
+        // Iterate over spaces
+        for (const space of contents.spaces) {
+            // Convert the space in an object
+            const space_obj = JSON.parse(space);
+
+            // Create an annotation for the space
+            this.addAnnotation(space_obj);
+
+            // Check if a space contains subspaces
+            if (space_obj.spaces.length > 0) {
+              // Create annotations for subspaces
+              this.makeAnnotations(space_obj);
+            }
+        }
+
+    }
+
+    // Save an annotation in memory
+    private addAnnotation(contents: RcaFile) {
+        let annotation: any = {
+            path: contents.filepath,
+            start_line: contents.line_start,
+            end_line: contents.line_end,
+            annotation_level: 'notice',
+            title: contents.filepath,
+            message: this.getAnnotationMetrics(contents.metrics),
+        };
+
+        // FIXME: Perhaps we should do something for spaces on a single line.
+        // Retrieving the file from GitHub and getting the length of the column?
+
+        this.annotations.push(annotation);
+    }
+
+
 
     // Return text shown in the annotation formatted as markdown
     private getText(rcaVersion: string): string {
@@ -228,60 +347,74 @@ ${this.file_metrics.join('\n')}`;
     // Return file metrics formatted as markdown
     private getFileMetrics(contents: RcaFile): string {
         return `<details>
-<summary><b>${contents.name}</b></summary>
+<summary><b>${contents.filepath}</b></summary>
 
-  Nargs: ${contents.metrics.nargs}
+${this.getMetrics(contents.metrics)}
+</details>`;
+    }
 
-  Nexits: ${contents.metrics.nexits}
+    // Return space metrics annotations formatted as markdown
+    private getAnnotationMetrics(metrics: Metrics): string {
+        return `<details>
+<summary><b>Space Metrics</b></summary>
 
-  Cognitive: ${contents.metrics.cognitive}
+${this.getMetrics(metrics)}
+</details>`;
+    }
+
+    // Returns metrics formatted as markdown
+    private getMetrics(metrics: Metrics): string {
+      return `Nargs: ${metrics.nargs}
+
+  Nexits: ${metrics.nexits}
+
+  Cognitive: ${metrics.cognitive}
   <details>
   <summary>Cyclomatic</summary>
 
-  - Sum: ${contents.metrics.cyclomatic.sum}
-  - Average: ${contents.metrics.cyclomatic.average}
+  - Sum: ${metrics.cyclomatic.sum}
+  - Average: ${metrics.cyclomatic.average}
   </details>
   <details>
   <summary>Loc</summary>
 
-  - Sloc: ${contents.metrics.loc.sloc}
-  - Ploc: ${contents.metrics.loc.ploc}
-  - Lloc: ${contents.metrics.loc.lloc}
-  - Cloc: ${contents.metrics.loc.cloc}
-  - Blank: ${contents.metrics.loc.blank}
+  - Sloc: ${metrics.loc.sloc}
+  - Ploc: ${metrics.loc.ploc}
+  - Lloc: ${metrics.loc.lloc}
+  - Cloc: ${metrics.loc.cloc}
+  - Blank: ${metrics.loc.blank}
   </details>
   <details>
   <summary>Nom</summary>
 
-  - Functions: ${contents.metrics.nom.functions}
-  - Closures: ${contents.metrics.nom.closures}
-  - Total: ${contents.metrics.nom.total}
+  - Functions: ${metrics.nom.functions}
+  - Closures: ${metrics.nom.closures}
+  - Total: ${metrics.nom.total}
   </details>
   <details>
   <summary>Halstead</summary>
 
-  - n1: ${contents.metrics.halstead.n1}
-  - N1: ${contents.metrics.halstead.N1}
-  - n2: ${contents.metrics.halstead.n2}
-  - N2: ${contents.metrics.halstead.N2}
-  - Length: ${contents.metrics.halstead.length}
-  - Estimated program length: ${contents.metrics.halstead.estimated_program_length}
-  - Purity ratio: ${contents.metrics.halstead.purity_ratio}
-  - Vocabulary: ${contents.metrics.halstead.vocabulary}
-  - Volume: ${contents.metrics.halstead.volume}
-  - Difficulty: ${contents.metrics.halstead.difficulty}
-  - Level: ${contents.metrics.halstead.level}
-  - Effort: ${contents.metrics.halstead.effort}
-  - Time: ${contents.metrics.halstead.time}
-  - Bugs: ${contents.metrics.halstead.bugs}
+  - n1: ${metrics.halstead.n1}
+  - N1: ${metrics.halstead.N1}
+  - n2: ${metrics.halstead.n2}
+  - N2: ${metrics.halstead.N2}
+  - Length: ${metrics.halstead.length}
+  - Estimated program length: ${metrics.halstead.estimated_program_length}
+  - Purity ratio: ${metrics.halstead.purity_ratio}
+  - Vocabulary: ${metrics.halstead.vocabulary}
+  - Volume: ${metrics.halstead.volume}
+  - Difficulty: ${metrics.halstead.difficulty}
+  - Level: ${metrics.halstead.level}
+  - Effort: ${metrics.halstead.effort}
+  - Time: ${metrics.halstead.time}
+  - Bugs: ${metrics.halstead.bugs}
   </details>
   <details>
   <summary>Maintainability Index</summary>
 
-  - Original: ${contents.metrics.mi.mi_original}
-  - Visual studio: ${contents.metrics.mi.mi_visual_studio}
-  - Sei: ${contents.metrics.mi.mi_sei}
-  </details>
-</details>`;
-    }
+  - Original: ${metrics.mi.mi_original}
+  - Visual studio: ${metrics.mi.mi_visual_studio}
+  - Sei: ${metrics.mi.mi_sei}
+  </details>`;
+      }
 }
